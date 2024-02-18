24e26746ed3c48b015b0d209c43476d70e048774
unknown <hdiisudarso@gmail.com>
2024-02-18 21:38:05 +0700

    first commit

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\buku;
+use App\Http\Controllers\Controller;
+
+
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $dtBuku = buku::orderBy('id', 'desc')->get();
+        return view('data_buku.index', compact('dtBuku'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('data_buku.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+        $request->validate(
+            [
+                'judul' => 'required',
+                'penulis' => 'required',
+                'penerbit' => 'required',
+                'tahun_terbit' => 'required',
+            ],
+            [
+                'judul.required' => 'judul wajib diisi',
+                'penulis.required' => 'penulis wajib diisi',
+                'penerbit.required' => 'penerbit wajib diisi',
+                'tahun_terbit.required' => 'tahun_terbit wajib diisi',
+            ],
+        );
+        $data = [
+            'judul' => $request->judul,
+            'penulis' => $request->penulis,
+            'penerbit' => $request->penerbit,
+            'tahun_terbit' => $request->tahun_terbit,
+        ];
+        buku::create($data);
+        return back()->with('success', 'successfully added data');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
